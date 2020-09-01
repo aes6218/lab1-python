668850906d2bb8cca8967e48595cb238f25318db
August Sanderson <aes6218@psu.edu>
2020-09-01 21:46:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+temp = float(input("Enter temperature: "))
+sys = input("Enter unit in F/f or C/c: ")
+
+if sys.lower()=="f":
+    print(f"{temp}° in Fahrenheit is equivalent to {(temp-32)-32}° Celsius.")
+elif sys.lower()=="c":
+    print(f"{temp}° in Celsius is equivalent to {(temp*9/5)+32}° Fahrenheit.")
+else:
+  print(f"Invalid unit((sys)).")
