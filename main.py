fdce7f173fd26568fbc6bcdc425e4b8c1b767489
August Sanderson <aes6218@psu.edu>
2020-09-04 18:59:11 +0000

    moved collab names

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Author: August Sanderson aes6218@psu.edu
+# Collaborator: Kirtan Shah kps5871@psu.edu
+# Collaborator: Anthony Cicardo abc6181@psu.edu
+# Collaborator: Karen Rodriguez kvr5509@psu.edu
+
 temp = float(input("Enter temperature: "))
 sys = input("Enter unit in F/f or C/c: ")
 
@@ -7,8 +12,3 @@ elif sys.lower()=="c":
     print(f"{temp}° in Celsius is equivalent to {(temp*9/5)+32}° Fahrenheit.")
 else:
   print(f"Invalid unit({sys}).")
-
-# Author: August Sanderson aes6218@psu.edu
-# Collaborator: Kirtan Shah kps5871@psu.edu
-# Collaborator: Anthony Cicardo abc6181@psu.edu
-# Collaborator: Karen Rodriguez kvr5509@psu.edu
